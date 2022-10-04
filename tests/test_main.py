ba51a8acaf7a6c46646814029b53d412cdeaa098
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-10-04 10:04:40 +0000

    tested vit model

--- tests/test_main.py
+++
@@ -1,8 +0,0 @@
-from src.main import find_sum
-
-
-def test_find_sum():
-    """Test for function find_sum.
-    """
-    s = find_sum(1, 1)
-    assert s == 2
--- vit/model.py
+++ vit/model.py
@@ -66,89 +66,83 @@ class PatchEmbed(nn.Module):
 
 class Attention(nn.Module):
     """Attention mechanism.
-
     Parameters
     ----------
     dim : int
-        The input and out dimension pf per token features.
-
+        The input and out dimension of per token features.
     n_heads : int
-        Number of attention heads
-
+        Number of attention heads.
     qkv_bias : bool
         If True then we include bias to the query, key and value projections.
-
     attn_p : float
         Dropout probability applied to the query, key and value tensors.
-
     proj_p : float
         Dropout probability applied to the output tensor.
-
     Attributes
     ----------
     scale : float
-        Normalizing constant for the dot product.
-
+        Normalizing consant for the dot product.
     qkv : nn.Linear
+        Linear projection for the query, key and value.
+    proj : nn.Linear
         Linear mapping that takes in the concatenated output of all attention
         heads and maps it into a new space.
-
-    attn_drop, prod_drop : nn.Dropout
+    attn_drop, proj_drop : nn.Dropout
         Dropout layers.
     """
 
-    def __init__(self, dim, n_heads, qkv_bias=True, attn_p=0., proj_p=0.):
-        super(Attention, self).__init__()
+    def __init__(self, dim, n_heads=12, qkv_bias=True, attn_p=0., proj_p=0.):
+        super().__init__()
         self.n_heads = n_heads
         self.dim = dim
         self.head_dim = dim // n_heads
-        self.scale = self.scale = self.head_dim ** -0.5  # to prevent extreme large values to softmax
+        self.scale = self.head_dim ** -0.5
 
-        self.qkv = nn.Linear(dim, dim*3, bias=qkv_bias)
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
         self.attn_drop = nn.Dropout(attn_p)
         self.proj = nn.Linear(dim, dim)
         self.proj_drop = nn.Dropout(proj_p)
 
     def forward(self, x):
-        """Run forward pass
-
-        Parameters:
-        -----------
-            x : torch.Tensor
-                Shape `(n_samples, n_patches + 1,dim)`.
-
+        """Run forward pass.
+        Parameters
+        ----------
+        x : torch.Tensor
+            Shape `(n_samples, n_patches + 1, dim)`.
         Returns
         -------
         torch.Tensor
-            Shape`(n_samples, n_patches + 1,dim)`.
+            Shape `(n_samples, n_patches + 1, dim)`.
         """
-        n_samples, n_tokens, dim = x.Shape
+        n_samples, n_tokens, dim = x.shape
 
         if dim != self.dim:
             raise ValueError
 
-        qkv = self.qkv(x)  # (n_samples, n_patches + 1,3 * dim)
-        qkv = self.reshape(
+        qkv = self.qkv(x)  # (n_samples, n_patches + 1, 3 * dim)
+        qkv = qkv.reshape(
             n_samples, n_tokens, 3, self.n_heads, self.head_dim
-        )  # (n_samples, n_patches + 1, 3, n_heads, head_dim)
+        )  # (n_smaples, n_patches + 1, 3, n_heads, head_dim)
         qkv = qkv.permute(
             2, 0, 3, 1, 4
-        )  # (3, n_samples, n_heads, n_patches+1, head_dim)
+        )  # (3, n_samples, n_heads, n_patches + 1, head_dim)
+
         q, k, v = qkv[0], qkv[1], qkv[2]
         k_t = k.transpose(-2, -1)  # (n_samples, n_heads, head_dim, n_patches + 1)
         dp = (
             q @ k_t
         ) * self.scale  # (n_samples, n_heads, n_patches + 1, n_patches + 1)
         attn = dp.softmax(dim=-1)  # (n_samples, n_heads, n_patches + 1, n_patches + 1)
+        attn = self.attn_drop(attn)
 
-        weighted_avg = attn @ v  # (n_samples, n_heads, n_patches + 1, head_dim)
+        weighted_avg = attn @ v  # (n_samples, n_heads, n_patches +1, head_dim)
         weighted_avg = weighted_avg.transpose(
             1, 2
-        )  # (n_samples, n_patches +1, n_heads, head_dim)
-        weighted_avg = weighted_avg.flatten(2)  # (n_samples, n_patches +1, dim)
+        )  # (n_samples, n_patches + 1, n_heads, head_dim)
+        weighted_avg = weighted_avg.flatten(2)  # (n_samples, n_patches + 1, dim)
 
-        x = self.proj(weighted_avg)  # (n_samples, n_patches +1, dim)
-        x = self.proj_drop(x)  # (n_samples, n_patches +1 ,dim)
+        x = self.proj(weighted_avg)  # (n_samples, n_patches + 1, dim)
+        x = self.proj_drop(x)  # (n_samples, n_patches + 1, dim)
 
         return x
 
